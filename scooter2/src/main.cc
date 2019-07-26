f83a8c368e4b4589c8ba5b5b8ff18df8472fc0db
Kyle Isom <kyle@imap.cc>
2019-07-26 19:20:00 +0000

    Let's see if this works.

--- scooter2/src/main.cc
+++ scooter2/src/main.cc
@@ -9,7 +9,12 @@
 #include <HCSR04.h>
 
 
-double				 distance[3];
+constexpr float			 ClearanceDistance = 15.0;
+constexpr uint8_t		 DistanceLeft  = 0;
+constexpr uint8_t		 DistanceFront = 1;
+constexpr uint8_t		 DistanceRight = 2;
+float				 distance[3];
+
 sensors::HCSR04	 urs[3] = {
 	sensors::HCSR04(10, 11), // Left
 	sensors::HCSR04(8, 9),   // Forward
@@ -32,13 +37,22 @@ senseRange()
 static void
 printDistances()
 {
-	Serial << "DISTANCES" << endl;
-	Serial << "Left: "  << distance[0] << " cm" << endl;
-	Serial << "Front: " << distance[1] << " cm" << endl;
-	Serial << "Right: " << distance[2] << " cm" << endl;
+	uint8_t	i = 0;
+	while (true) {
+		Serial << "DISTANCES" << endl;
+		Serial << "Left: "  << distance[0] << " cm" << endl;
+		Serial << "Front: " << distance[1] << " cm" << endl;
+		Serial << "Right: " << distance[2] << " cm" << endl;
+		
+		for (i = 0; i < 250; i++) {
+			delay(1);
+			yield();
+		}
+	}
 }
 
 
+#ifdef DRIVE_TEST
 static void
 driveTest()
 {
@@ -78,6 +92,30 @@ driveTest()
 	Serial.println("hard stop");
 	Drive::Stop(true);
 }
+#endif // DRIVE_TEST
+
+
+static void
+avoidObstacles()
+{
+	// If an object is detected, start avoiding by choosing whether
+	// to go left or right. Keep turning until the front sensor
+	// reports that it is clear of obstacles, then resume driving.
+	if (distance[1] < ClearanceDistance) {
+		if (distance[0] < distance[2]) {
+			Drive::TurnRight();
+		}
+		else {
+			Drive::TurnLeft();
+		}
+
+		// The turning delay needs to make sure to relinquish
+		// control so that the sensors continue to update.
+		while (distance[1] < ClearanceDistance) yield();
+
+		Drive::Forward();
+	}
+}
 
 
 void
@@ -90,15 +128,37 @@ setup()
 	Drive::Begin();
 
 	delay(1000);
-	driveTest();
 
+#ifdef DRIVE_TEST
+	driveTest();
+#else //  DRIVE_TEST
 	Scheduler.startLoop(senseRange);
+	if (Serial) {
+		Scheduler.startLoop(printDistances);
+	}
+	Scheduler.startLoop(avoidObstacles);
+#endif // DRIVE_TEST
 }
 
 
 void
 loop()
 {
-	printDistances();
-	delay(250);
+	// If an object is detected, start avoiding by choosing whether
+	// to go left or right. Keep turning until the front sensor
+	// reports that it is clear of obstacles, then resume driving.
+	if (distance[DistanceFront] < 10.0) {
+		if (distance[DistanceLeft] < distance[DistanceRight]) {
+			Drive::TurnRight();
+		}
+		else {
+			Drive::TurnLeft();
+		}
+
+		// The turning delay needs to make sure to relinquish
+		// control so that the sensors continue to update.
+		while (distance[DistanceFront] < 10.0) yield();
+
+		Drive::Forward();
+	}
 }
