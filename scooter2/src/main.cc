b01522558c3cdb82bacfc65760a1daa5a23856bf
Kyle Isom <kyle@imap.cc>
2019-07-27 10:16:36 -0700

    Add logging and whatnot.

--- scooter2/src/main.cc
+++ scooter2/src/main.cc
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <SD.h>
 #include <Wire.h>
 
 #include <Adafruit_MotorShield.h>
@@ -11,7 +13,7 @@
 
 
 constexpr float			 ClearanceDistance = 30.0;
-constexpr uint8_t		 maxSteps = 8;
+constexpr uint8_t		 maxSteps = 9;
 uint8_t				 steps[maxSteps+1] = {
 	30, 45, 60, 75, 90, 105, 120, 135, 150,
 };
@@ -20,6 +22,27 @@ float				 distance[maxSteps];
 
 Servo				 ursPan;
 sensors::HCSR04			 urs(6, 7);
+
+constexpr uint8_t		 sdCS = 8;
+static bool			 loggingEnabled = false;
+
+#define logFile			 SEROUT
+
+
+static void
+logDistance()
+{
+	if (!loggingEnabled) {
+		return;
+	}
+
+	auto now = millis();
+	logFile << now << ",DIST";
+	for (uint8_t i = 0; i < maxSteps; i++) {
+		logFile << "," << distance[i];
+	}
+	logFile << endl;
+}
 
 
 static void
@@ -43,6 +66,19 @@ senseRange()
 		distance[i] /= 2;
 		delay(sensors::HCSR04::updateDelay);
 	}
+
+	logDistance();
+}
+
+
+static void
+logGuess(char direction, float distance)
+{
+	if (!loggingEnabled) {
+		return;
+	}
+	auto now = millis();
+	logFile << now << ",GUESS," << direction << "," << distance << endl;
 }
 
 
@@ -61,8 +97,10 @@ guessFrontDistance(bool useAverage=false)
 	}
 
 	if (useAverage) {
+		logGuess('F', average);
 		return average;
 	}
+	logGuess('F', minDist);
 	return minDist;
 }
 
@@ -84,8 +122,10 @@ guessLeftDistance(bool useAverage=false)
 	}
 
 	if (useAverage) {
+		logGuess('L', average);
 		return average;
 	}
+	logGuess('L', minDist);
 	return minDist;
 }
 
@@ -107,8 +147,10 @@ guessRightDistance(bool useAverage=false)
 	}
 
 	if (useAverage) {
+		logGuess('R', average);
 		return average;
 	}
+	logGuess('R', minDist);
 	return minDist;
 }
 
@@ -174,14 +216,60 @@ driveTest()
 #endif // DRIVE_TEST
 
 
+const uint8_t	STATE_NORMAL = 0;
+const uint8_t	STATE_AVOID = 1;
+const uint8_t	STATE_BACK = 2;
+const uint8_t	STATE_STOP = 3;
+const uint8_t	STATE_TURNLEFT = 4;
+const uint8_t	STATE_TURNRIGHT = 5;
+
+
+static void
+logStateChange(uint8_t state)
+{
+	if (!loggingEnabled) {
+		return;
+	}
+
+	auto now = millis();
+
+	logFile << now << "," << "STATE,";
+	switch (state) {
+	case STATE_NORMAL:
+		logFile << "DRIVE";
+		break;
+	case STATE_AVOID:
+		logFile << "AVOID";
+		break;
+	case STATE_BACK:
+		logFile << "BACK";
+		break;
+	case STATE_STOP:
+		logFile << "STOP";
+		break;
+	case STATE_TURNLEFT:
+		logFile << "TURN_LEFT";
+		break;
+	case STATE_TURNRIGHT:
+		logFile << "TURN_RIGHT";
+		break;
+	default:
+		logFile << "UNKNOWN_" << int(state);
+	}
+
+	logFile << endl;
+}
+
+
 void
 setup()
 {
 	Serial.begin(9600);
 	delay(1000);
 
-	Serial.println("setting up motors");
 	Drive::Begin();
+
+	loggingEnabled = true;
 
 	delay(1000);
 
@@ -190,12 +278,9 @@ setup()
 #else //  DRIVE_TEST
 	ursPan.attach(9);
 	Scheduler.startLoop(senseRange);
-	if (Serial) {
-		Scheduler.startLoop(printDistances);
-		Serial << "driving at " << int(Drive::DefaultSpeed) << "..." << endl;
-	}
 	Drive::SetSpeed(Drive::DefaultSpeed);
 	Drive::Forward();
+	logStateChange(STATE_NORMAL);
 	delay(1000); // gives the sensors time to start getting readings.
 #endif // DRIVE_TEST
 
@@ -209,37 +294,44 @@ loop()
 	// to go left or right. Keep turning until the front sensor
 	// reports that it is clear of obstacles, then resume driving.
 	if (guessFrontDistance() < ClearanceDistance) {
+		logStateChange(STATE_AVOID);
+
 		// Hard stop, backup, and make sure we turn quickly.
 		Drive::Stop();
+		logStateChange(STATE_STOP);
 		delay(500);
 
 		Drive::SetSpeed(2 * Drive::DefaultSpeed);
+		logStateChange(STATE_BACK);
 		Drive::Backward();
 		delay(250);
 
 		Drive::Stop();
-		Drive::SetSpeed(2 * Drive::DefaultSpeed);
+		logStateChange(STATE_STOP);
+		Drive::SetSpeed(Drive::DefaultSpeed);
 
 		// Pick the direction with the most room.
 		if (Serial) Serial << "AVOID" << endl;
 		if (guessLeftDistance() < guessRightDistance()) {
 			Serial << "turn right" << endl;
 			Drive::TurnRight();
+			logStateChange(STATE_TURNRIGHT);
 		}
 		else {
 			Serial << "turn left" << endl;
 			Drive::TurnLeft();
+			logStateChange(STATE_TURNLEFT);
 		}
 
 		// The turning delay needs to make sure to relinquish
 		// control so that the sensors continue to update.
 		while (guessFrontDistance() < 2 * ClearanceDistance) {
-			Serial << "turning" << endl;
 			yield();
 		}
 
 		Drive::SetSpeed(Drive::DefaultSpeed);
 		Drive::Forward();
+		logStateChange(STATE_NORMAL);
 	}
 	yield();
 }
